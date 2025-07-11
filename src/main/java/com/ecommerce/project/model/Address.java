5689fa550c1774f554f434491cb1697f0823bb71
S-SuryaPrakash <s.suryaprakash7767@gmail.com>
2025-07-11 18:02:38 +0530

    manage user profile along with role and address

---
+++ src/main/java/com/ecommerce/project/model/Address.java
@@ -0,0 +1,52 @@
+package com.ecommerce.project.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "addresses")
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long addressId;
+    @NotBlank
+    @Size(min=5, message = "street min 5 char")
+    private String street;
+
+    @NotBlank
+    private String buildingName;
+
+    @NotBlank
+    private String city;
+
+    @NotBlank
+    private String state;
+
+    @NotBlank
+    private String country;
+
+    @NotBlank
+    private String pincode;
+
+    @ManyToMany(mappedBy = "addresses")
+    private List<User> users = new ArrayList<User>();
+
+    public Address(String street, String buildingName, String city, String state, String country, String pincode, List<User> users) {
+        this.street = street;
+        this.buildingName = buildingName;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+        this.pincode = pincode;
+    }
+}
